0dea43d87f43ae6e93d44f1723238ccd7b857fa4
adeha047 <dehaven7777@gmail.com>
2021-06-24 11:25:32 -0400

    added new updates

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -25,7 +25,39 @@ const ProductScreen = ({ match }) => {
                     <Rating value={product.rating} text={`${product.numReviews} 
                     reviews`} /> 
                 </ListGroup.Item>
+                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                <ListGroup.Item>Description: {product.description}</ListGroup.Item>
             </ListGroup>
+        </Col>
+        <Col md={3}>
+            <Card>
+                <ListGroup variant="flush"></ListGroup>
+                <ListGroup.Item>
+                    <Row>
+                        <Col>Price:</Col>
+                        <Col>
+                        <strong>${product.price}</strong>
+                        </Col>
+                    </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Row>
+                        <Col>Status:</Col>
+                        <Col>
+                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                        </Col>
+                    </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Button 
+                    className='btn-block' 
+                    type='button'
+                    disabled={product.countInStock === 0 }
+                    >
+                        Add To Cart
+                    </Button>
+                </ListGroup.Item>
+            </Card>
         </Col>
       </Row>
     </>
