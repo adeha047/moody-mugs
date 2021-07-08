40e288ef5dc13669ee28e50252c60a3fe0893f1a
adeha047 <dehaven7777@gmail.com>
2021-07-08 15:49:32 -0400

    added new seeder.js file

--- JS-backend/seeder.js
+++ JS-backend/seeder.js
@@ -20,10 +20,39 @@ const importData = async () => {
 
         const createdUsers = await User.insertMany(users)
 
-        const adminUser = createdUsers[0]_id
+        const adminUser = createdUsers[0]._id
+
+        const sampleProducts = products.map(product => {
+            return {...product, user: adminUser}
+        })
+
+        await Product.insertMany(sampleProducts)
+        console.log('Data Imported Successfully')
+        process.exit()
     } catch (error) {
+        console.log(`${error}`)
+        process.exit(1)
 
     }
+}
 
+const destroyData = async () => {
+    try {
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
 
+        console.log('Data Destroyed')
+        process.exit()
+    } catch (error) {
+        console.log(`${error}`)
+        process.exit(1)
+
+    }
+}
+
+if (process.argv[2] === '-d') {
+    destroyData()
+} else {
+    importData()
 }
