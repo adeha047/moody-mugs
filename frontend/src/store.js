fb5961c3e09f7395ee2c1303be87ab6700681be3
adeha047 <dehaven7777@gmail.com>
2021-07-12 11:02:50 -0400

    added updates in index.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { createStore, createReducers, applyMiddleware } from 'react'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { middleware } from 'yargs'
 
 const reducer = combineReducers({})
 
 const initialState = {}
 
-const store = createStore(reducer, initalState)
+const store = createStore(reducer, initalState, composeWithDevTools(applyMiddleware([...middleware])))
+
+export default store
