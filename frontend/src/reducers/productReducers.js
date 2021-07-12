e9eab68cf5ba6a3f28bc374dd26cedfffd716ccb
adeha047 <dehaven7777@gmail.com>
2021-07-12 13:58:28 -0400

    added new files

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,3 +1,11 @@
-export const productReducer = () => {
-    
+export const productReducer = (state = { products:[]}, action ) => {
+    switch (action.type) {
+        case 'PRODUCT_LIST_REQUEST': 
+        return {loading: true, products: []}
+        case 'PRODUCT_LIST_SUCCESS': 
+        return {loading: true, products: action.payload }
+        case 'PRODUCT_LIST_REQUEST': 
+        return {loading: true, products: []}
+    }
+
 }
