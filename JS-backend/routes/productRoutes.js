520a217babb563ce00b26731347a2e3e372f02c0
adeha047 <dehaven7777@gmail.com>
2021-07-14 10:53:48 -0400

    acdded new files in back end'

--- JS-backend/routes/productRoutes.js
+++ JS-backend/routes/productRoutes.js
@@ -1,20 +1,25 @@
 import express from 'express'
-import expressAsyncHandler from 'expressAsyncHandler'
+import asyncHandler from 'expressAsyncHandler'
 const router = express.Router()
-import Product from '../models/productModel'
+import Product from '../models/productModel.js'
 
 
-
-router.get('/', async (req, res) => {
+// Fetch all products GET /api/routes
+router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({})
     res.json(products);
 
-})
+}))
+// Fetch one products GET /api/routes
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    
+    if(product) {
+        res.json(product)
+    } else {
+        res.status(404).json({message: 'Product not found'})
+    }
 
-router.get('/:id', (req, res) => {
-    const product = products.find(p => p.id === req.params.id)
-    res.json(product);
-
-})
+}))
 
 export default router
