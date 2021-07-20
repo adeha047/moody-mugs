c0fa85bd197bf60c4a9dfe05eadbf0a5fe4e4ad1
adeha047 <dehaven7777@gmail.com>
2021-07-20 16:50:45 -0400

    added new reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,6 @@
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from '../constants/productConstants'
 
-export const productReducer = (state = { products: [] }, action) => {
+export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,14 @@ import { createStore, createReducers, applyMiddleware } from 'react'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+
 import { middleware } from 'yargs'
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer, 
+    cart: cartReducer 
 })
 
 const initialState = {}
