d0f6497cc5ae587cecae7ca7b2151c16e49aed7f
adeha047 <dehaven7777@gmail.com>
2021-07-26 14:50:58 -0400

    adde updated cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -25,6 +25,9 @@ const CartScreen = ({ match, location, history }) => {
     const removeFromCart = (id) => {
         console.log('remove')
     }
+    const checkoutHandler = () => {
+        console.log('checkout')
+    }
     
     return (
         <Row>
@@ -72,6 +75,11 @@ const CartScreen = ({ match, location, history }) => {
                         <ListGroup.Item>
                             <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
                             ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>
+                                Proceed To Checkout
+                            </Button>
                         </ListGroup.Item>
                     </ListGroup>
                 </Card>
