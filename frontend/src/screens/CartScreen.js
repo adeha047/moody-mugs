b47ee362b496e6470875c74181cea34462d7b71d
adeha047 <dehaven7777@gmail.com>
2021-07-25 21:55:25 -0400

    added new trash bin for cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -35,6 +35,26 @@ const CartScreen = ({ match, location, history }) => {
                                         src={item.image} alt={item.name} fluid rounded
                                     />
                                 </Col>
+                                <Col md={3}>
+                                    <Link to={`/product/${item.product}`}>{itme.name}</Link>
+                                </Col>
+                                <Col md={2}>{item.price}</Col>
+                                <Col md={2}> <Form.Control as='select' value={qty} onChange={(e)  => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                      {[...Array(item.countInStock).keys()].map(x => (
+                        <option key={x +1} value={x + 1}>
+                          {x +1}
+                        </option>
+                      ))}
+                      </Form.Control>
+                      </Col>
+                      <Col md={2}>
+                          <Button type='button' variant='light' onClick={() => removeFromCartHandler(item.product)}>
+                              <i className='fas fa-trash'></i>
+
+                          </Button>
+
+                      </Col>
+
                             </Row>
                         </ListGroup.Item>
 
