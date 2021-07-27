e2c1ec148ed99539d747be9f9dbd7b8e6766925e
adeha047 <dehaven7777@gmail.com>
2021-07-26 21:43:26 -0400

    added new cart info

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -12,7 +12,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
         }
 
         case CART_REMOVE_ITEM: 
-        return { ...state, cartItems: state.cartItems.filter(x => x.product != action.payload),
+        return { ...state, cartItems: state.cartItems.filter((x) => x.product != action.payload),
         }
         default: 
         return state
