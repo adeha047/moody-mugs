c03939dc8b94966e5f314502f82ee23cf87c8ca2
adeha047 <dehaven7777@gmail.com>
2021-07-29 15:24:05 -0400

    added new files

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,15 @@
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants";
+
+
+export const userLoginReducer = (state = { }, action) => {
+    switch (action.type) {
+      case USER_LOGIN_REQUEST:
+        return { loading: true, USERs: [] };
+      case USER_LOGIN_SUCCESS:
+        return { loading: false, USER: action.payload };
+      case USER_LOGIN_FAIL:
+        return { loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  }
