5de27dc5405124b1b4bddc5480fdd9d3ca1f716d
adeha047 <dehaven7777@gmail.com>
2021-08-02 12:13:09 -0400

    added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -31,3 +31,8 @@ export const login = (email, password) => async (dispatch) => {
     }
 
 }
+
+export const logout = () => (dispatch) => {
+    localstorage.removeItem('userInfo')
+    dispatch({ type: USER_LOGOUT })
+}
