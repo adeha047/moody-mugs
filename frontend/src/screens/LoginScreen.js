b05bf3a9d3d7607229c81256c6f4c3d8127f7be4
adeha047 <dehaven7777@gmail.com>
2021-08-02 11:02:41 -0400

    added new updates to login screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,20 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import Message from "../components/Message";
-import Loader from "../components/Loader";
-import FormContainer from "../components/FormContainer";
-import { login } from "actions/userActions";
+import { Form, Button, Row, Col } from "react-bootstrap"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+import FormContainer from "../components/FormContainer"
+import { login } from "actions/userActions"
 
-const loginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const loginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const dispatch = useDispatch()
+  
+  const userLogin = useSelector(state => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+  
   const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+      if(userInfo) {
+          history.push(redirect)
+      }
+  }, [ history, userInfo, redirect ])
+
+  const submitHandler = (e) => {
+      e.preventDefault()
+      dispatch(login(email, password))
+  }
+
 
   return (
     <FormContainer>
       <h1> Sign In </h1>
+      {error && <Message variant='danger'> {error}</Message>}
+      {loading && <Loader />}
       <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label>Email Address</Form.Label>
