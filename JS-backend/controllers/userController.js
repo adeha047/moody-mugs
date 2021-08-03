4ca706ae68c37dfaa23f469d86ba866f0b9f3d8d
adeha047 <dehaven7777@gmail.com>
2021-08-03 10:55:47 -0400

    added new user routes

--- JS-backend/controllers/userController.js
+++ JS-backend/controllers/userController.js
@@ -68,4 +68,29 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if(req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.isAdmin,
+      token: generateToken(updatedUser_id),
+    })
+  } else {
+    res.status(404)
+    throw new Error("User Not Found");
+  }
+})
+
 export { authUser, registerUser, getUserProfile };
