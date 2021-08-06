ff78960c79fe88311d4f72400c7aaacdf8467fd9
adeha047 <dehaven7777@gmail.com>
2021-08-06 15:11:44 -0400

    added new payment screen

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -16,6 +16,39 @@ const CheckoutSteps = () => {
                )}
            </Nav.Item>
 
+           <Nav.Item>
+               {step2 ? (
+
+                   <LinkContainer to='/shipping'> 
+                   <Nav.Link>Shipping</Nav.Link>
+                   </LinkContainer>
+               ) : (
+                   <Nav.Link disabled>Shipping</Nav.Link>
+               )}
+           </Nav.Item>
+
+           <Nav.Item>
+               {step3 ? (
+
+                   <LinkContainer to='/payment'> 
+                   <Nav.Link>Payment</Nav.Link>
+                   </LinkContainer>
+               ) : (
+                   <Nav.Link disabled>Payment</Nav.Link>
+               )}
+           </Nav.Item>
+
+           <Nav.Item>
+               {step4 ? (
+
+                   <LinkContainer to='/placeorder'> 
+                   <Nav.Link>Place Order</Nav.Link>
+                   </LinkContainer>
+               ) : (
+                   <Nav.Link disabled>Place Order</Nav.Link>
+               )}
+           </Nav.Item>
+
        </Nav>
     )
 }
