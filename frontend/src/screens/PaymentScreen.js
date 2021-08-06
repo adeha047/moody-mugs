a2b6166ff99b4555e70457c1c9dba40471d9bcc8
adeha047 <dehaven7777@gmail.com>
2021-08-06 15:25:50 -0400

    added new functionality in payment screen

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,76 +1,50 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer"
 import CheckoutSteps from "../components/CheckoutSteps"
-import { saveShippingAddress } from '../actions/cartActions'
+import { savePaymentMethod } from '../actions/cartActions'
 
-const ShippingScreen = ({ history }) => {
+const PaymentScreen = ({ history }) => {
     const cart = useSelector(state => state.cart)
     const { shippingAddress } = cart
 
+    if(!shippingAddress) {
+        history.push('/shipping')
+    }
 
-  const [address, setAddress] = useState(shippingAddress.state);
-  const [city, setCity] = useState(shippingAddress.state);
-  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
-  const [country, setCountry] = useState(shippingAddress.country)
+
+  const [paymentMethod, setPaymentMethod] = useState('PayPal')
+  
 
   const dispatch = useDispatch()
   
   const submitHandler = (e) => {
       e.preventDefault()
-      dispatch(saveShippingAddress({ address, city, postalCode, country }))
-      history.push('/payment')
+      dispatch(savePaymentMethod(paymentMethod))
+      history.push('/placeorder')
       
   }
 
   return (
     <FormContainer>
-      <CheckoutSteps step1 step2/>
-      <h1>Shipping</h1>
+      <CheckoutSteps step1 step2 step3/>
+      <h1>Payment Method</h1>
       <Form>
         {" "}
         onSubmit={submitHandler}
-        <Form.Group controlId="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter name"
-            value={address}
-            required
-            onChange={(e) => setAddress(e.target.value)}
-          ></Form.Control>
+        <Form.Group>
+            <Form.Check 
+            type='radio'
+            label='PayPal or Credit Card'
+            id='PayPal'
+            name='paymentMethod'
+            value='PayPal'
+            checked
+            onChange={e => setPaymentMethod(e.target.value)}
+            ></Form.Check>
         </Form.Group>
-        <Form.Group controlId="city">
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter city"
-            value={city}
-            required
-            onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="postalCode">
-          <Form.Label>postal Code</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Postal Code"
-            value={postalCode}
-            required
-            onChange={(e) => setPostalCode(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="country">
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Country"
-            value={country}
-            required
-            onChange={(e) => setCountry(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+        
 
         <Button type="submit" variant="primary">
           Continue
@@ -80,4 +54,4 @@ const ShippingScreen = ({ history }) => {
   );
 };
 
-export default ShippingScreen
+export default PaymentScreen
